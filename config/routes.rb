6e8fc00e90b8ebf766dac85716686a1af3b89435
DevilSOF <33479896+DevilSOF@users.noreply.github.com>
2018-06-15 14:10:08 +0500

    post auth add and config changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
+  devise_for :users
   ActiveAdmin.routes(self)
   get 'posts/index'
 
