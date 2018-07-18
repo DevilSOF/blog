0cfd2c8199553dbfffde63870ab85e1c22d8649a
DevilSOF <soldiersof@gmail.com>
2018-07-18 11:39:50 +0500

    sign up username added, minor views fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :posts
   has_many :comments
 end
